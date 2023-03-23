5565db63480dfa91f117a2bebb69637b31429ea1
Charmila <charmilasimbine@gmail.com>
2023-03-23 13:47:51 +0200

    commit

--- users/index.php
+++ users/index.php
@@ -27,54 +27,54 @@ include_once("../header.php"); ?>
 			<div class="col-lg-12 stretch-card">
                 <div class="card">
                   <div class="card-body">
-                    <h4 class="card-title">Table with contextual classes</h4>
+                    <h4 class="card-title">Lista dos Usuarios</h4>
                     <p class="card-description"> Add class <code>.table-{color}</code>
                     </p>
                     <table class="table table-bordered">
                       <thead>
                         <tr>
                           <th> # </th>
-                          <th> First name </th>
-                          <th> Product </th>
-                          <th> Amount </th>
-                          <th> Deadline </th>
+                          <th> Nome </th>
+                          <th> Curso </th>
+                          <th> Ano </th>
+                          <th> Codigo de estudante </th>
                         </tr>
                       </thead>
                       <tbody>
                         <tr class="table-info">
                           <td> 1 </td>
-                          <td> Herman Beck </td>
-                          <td> Photoshop </td>
-                          <td> $ 77.99 </td>
-                          <td> May 15, 2015 </td>
+                          <td> Charmila Simbine </td>
+                          <td> Informatica </td>
+                          <td> 3 </td>
+                          <td> 01.0375.2021 </td>
                         </tr>
                         <tr class="table-warning">
                           <td> 2 </td>
-                          <td> Messsy Adam </td>
-                          <td> Flash </td>
-                          <td> $245.30 </td>
-                          <td> July 1, 2015 </td>
+                          <td> Nelma Binguane </td>
+                          <td> Biologia </td>
+                          <td> 4 </td>
+                          <td> 01.0523.2020 </td>
                         </tr>
                         <tr class="table-danger">
                           <td> 3 </td>
-                          <td> John Richards </td>
-                          <td> Premeire </td>
-                          <td> $138.00 </td>
-                          <td> Apr 12, 2015 </td>
+                          <td> Jose Nuvunga </td>
+                          <td> Ensino Basico</td>
+                          <td> 3 </td>
+                          <td> 01.2456.2021 </td>
                         </tr>
                         <tr class="table-success">
                           <td> 4 </td>
-                          <td> Peter Meggik </td>
-                          <td> After effects </td>
-                          <td> $ 77.99 </td>
-                          <td> May 15, 2015 </td>
+                          <td> Jossefina Sitoe </td>
+                          <td> Psicologia </td>
+                          <td> 1</td>
+                          <td> 01.6738.2023 </td>
                         </tr>
                         <tr class="table-primary">
                           <td> 5 </td>
-                          <td> Edward </td>
-                          <td> Illustrator </td>
-                          <td> $ 160.25 </td>
-                          <td> May 03, 2015 </td>
+                          <td> Leonardo Lacerda</td>
+                          <td> Quimica </td>
+                          <td> 2 </td>
+                          <td> 01.4628.2022 </td>
                         </tr>
                       </tbody>
                     </table>
